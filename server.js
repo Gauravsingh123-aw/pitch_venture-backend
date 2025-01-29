935728915a45c08bbe76aed3f135bc0f5c8daf43
Gaurav Singh <gauravsinghaw123@gmail.com>
2025-01-29 19:13:50 +0530

    added some basic dependencies

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-const app=require('http')
+const exp=require('express')
+const app=exp()
+require('dotenv').config()
+
+//application level middleware
+app.use(exp.json())
+
+const PORT=process.env.PORT || 4000;
+app.listen(PORT,()=>console.log(`server is running on port ${PORT}`))
