c4142a24362522886e97521e25b22da951924e60
Gaurav Singh <gauravsinghaw123@gmail.com>
2025-01-31 08:59:08 +0530

    added a req.http to test the api's

--- server.js
+++ server.js
@@ -5,5 +5,14 @@ require('dotenv').config()
 //application level middleware
 app.use(exp.json())
 
+app.get('/users',(req,res)=>{
+    let data=req.body
+    res.send({payload:data,message:"request successful"})
+})
+//Error handling middleware
+app.use((err,req,res,next)=>{
+    res.send({error:err,payload:"error occured"});
+})
+
 const PORT=process.env.PORT || 4000;
 app.listen(PORT,()=>console.log(`server is running on port ${PORT}`))
