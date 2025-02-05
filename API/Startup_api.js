2ac068347dd67694af245ec1090c53dbd55afe96
Gaurav Singh <gauravsinghaw123@gmail.com>
2025-02-06 00:08:45 +0530

    added api to get startup detail

--- API/Startup_api.js
+++ API/Startup_api.js
@@ -2,7 +2,10 @@ const exp=require('express')
 const startupApp=exp.Router()
 const expressAsyncHandler=require('express-async-handler')
 
-const {registerStartup}=require('../Controller/Startup_cont.js')
+const {registerStartup,getStartupData}=require('../Controller/Startup_cont.js')
 startupApp.post('/register_startup',expressAsyncHandler(registerStartup))
+
+//getting startup data
+startupApp.post('/get_startupdata',expressAsyncHandler(getStartupData));
 
 module.exports=startupApp;
--- Controller/Startup_cont.js
+++ Controller/Startup_cont.js
@@ -1,7 +1,33 @@
+const {Startup,User}=require('../db.js')
+
+// register startup
 async function registerStartup(req,res){
-   let body=req.body
-   console.log(body)
-   res.send(body)
+   let body=req.body.formData
+   let search=await Startup.find({startup_id:body.startup_id})
+   if(search.length===0){
+    let searchUser=await User.find({username:body.username});
+    if(searchUser.length!=0){
+        let ans=await Startup.create(body);
+        res.status(201).send({message:"startup listed",payload:ans})
+    }
+    else{
+        res.send({message:"username does not exist"})
+    }
+   }
+   else{
+    res.send({message:"startup id exists"});
+   }
 }
 
-module.exports={registerStartup}
+// get startup data
+async function getStartupData(req,res){
+    let body=req.body
+    let ans=await Startup.find({username:body.data})
+    if(ans.length!=0){
+        res.status(201).send({payload:ans,message:"startup data found"});
+    }
+    else
+    res.send({message:"startup detail not found"});
+}
+
+module.exports={registerStartup,getStartupData}
