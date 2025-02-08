b263cadbc46c79885bff3a18b1a73dfc40346caa
Gaurav Singh <gauravsinghaw123@gmail.com>
2025-02-08 23:14:27 +0530

    added controller for investor

--- API/Investor_api.js
+++ API/Investor_api.js
@@ -1,4 +1,7 @@
 const exp=require('express')
 const investorApp=exp.Router()
+const  expressasynchandler=require('express-async-handler')
+const {checkInvestorDetail}=require('../Controller/Investor_cont.js')
+investorApp.post('/check_investor',expressasynchandler(checkInvestorDetail))
 
 module.exports=investorApp;
---
+++ Controller/Investor_cont.js
@@ -0,0 +1,7 @@
+const Investor=require('../db.js')
+async function checkInvestorDetail(req,res){
+    let body=req.body
+    console.log(body)
+}
+
+module.exports={checkInvestorDetail}
